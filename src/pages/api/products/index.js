33c71d41a0cd966000bda3dd0e8d349539a05762
Uzma-Aashiya <uzmaaasia444@gmail.com>
2026-03-14 12:41:10 +0530

    fix import path

--- src/pages/api/products/index.js
+++ src/pages/api/products/index.js
@@ -1,6 +1,6 @@
 // src/pages/api/products/index.js
-import dbConnect from "../../../../lib/dbConnect";
-import Product from "../../../../models/Product";
+import dbConnect from "../../../lib/dbConnect";
+import Product from "../../../models/Product";
 
 export default async function handler(req, res) {
   await dbConnect();
