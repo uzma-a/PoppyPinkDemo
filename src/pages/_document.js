04d041372b67ca16df8e0e76baa61f85f610611d
Uzma-Aashiya <uzmaaasia444@gmail.com>
2026-03-16 14:51:43 +0530

    add logo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
