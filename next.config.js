e33db18b0ce3c457abcc129975e62f22eb0c7276
Uzma-Aashiya <149761349+uzma-a@users.noreply.github.com>
2026-03-18 01:38:37 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,5 +4,16 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'img.clerk.com'],
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "header", key: "x-forwarded-proto", value: "http" }],
+        destination: "https://poppypinkshoes.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 }
+
 module.exports = nextConfig
